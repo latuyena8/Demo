985855027d0a4c5a177090abfa4a7e58f4902f40
latuyena8 <50898996+latuyena8@users.noreply.github.com>
2020-10-14 21:43:25 +0700

    first commit

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -11,8 +11,14 @@ module.exports.create = function (req, res) {
         res.json(product)
     })
 }
-module.exports.put = function (req, res) {    
-    Product.create(req.body).then(function(product){
-        res.json(product)
-    })
-}
+// module.exports.put = function (req, res) { 
+//     var newvalues = {name: "IPhone PUT new PUT", description: "IPhone PUT newPUT" } ;
+//     Product.updateOne({name: "IPhone PUT"},newvalues).then(function(product){
+//         res.json(product)
+//     })
+// }
+// module.exports.delete = function (req, res) {    
+//     Product.delete(req.body).then(function(product){
+//         res.json(product)
+//     })
+// }
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -3,4 +3,6 @@ var router = express.Router()
 var controller = require('../controllers/product.controller')
 router.get('/',controller.index)
 router.post('/',controller.create)
+// router.put('/',controller.put)
+// router.delete('/',controller.delete)
 module.exports = router
